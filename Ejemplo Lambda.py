eecf5e3de9ba1c04644090b88d8fe186f7b72409
Carlos Basulto <84686010+cbasultog-ipn@users.noreply.github.com>
2021-05-24 10:56:14 -0500

    Add files via upload

---
+++ Ejemplo Lambda.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 10:34:14 2021
+
+@author: carlosbasulto
+"""
+
+f = lambda x: x*2
+
+print("Ingresa un número a multiplicar por 2")
+x = int(input())
+print (f(x))
